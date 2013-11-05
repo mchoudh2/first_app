b79a456a9d7380c7471e07c14cc3b023431f53f9
mseervi <mseervi@airbiquity.com>
2013-11-04 16:18:13 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ba9359b1a30d01ab2d330391e44d14b5ab68ebef49db6b790cad9acb41fbb2ec9608a4da4db836c0143186f315bf3cb48d1e5dde29e9befde2791e6c9297286b'
